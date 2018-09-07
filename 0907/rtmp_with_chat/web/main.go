d3f28490859da7ddb8223b9d534441cd8daa1e5f
assly <blue20will@gmail.com>
2018-09-07 10:48:52 +0900

    add: Initial commit of 09/07

---
+++ 0907/rtmp_with_chat/web/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/flosch/pongo2"
+	"github.com/labstack/echo"
+)
+
+var (
+	manager LiveManager
+)
+
+type LiveInfo struct {
+	app  string
+	name string
+}
+
+func (i LiveInfo) key() string {
+	return fmt.Sprintf("app=%s&name=%s", i.app, i.name)
+}
+
+func (i LiveInfo) URL() string {
+	return fmt.Sprintf("http://localhost:8081/hls/%s.m3u8", i.name)
+}
+
+type LiveManager struct {
+	publish chan *LiveInfo
+	done    chan *LiveInfo
+	lives   map[string]*LiveInfo
+}
+
+func (m *LiveManager) run() {
+	for {
+		select {
+		case info := <-m.publish:
+			manager.lives[info.key()] = info
+		case info := <-m.done:
+			if _, ok := manager.lives[info.key()]; ok {
+				delete(manager.lives, info.key())
+			}
+		}
+	}
+}
+
+var templateIndex = pongo2.Must(pongo2.FromFile("template/index.html"))
+
+func main() {
+	manager = LiveManager{
+		publish: make(chan *LiveInfo, 64),
+		done:    make(chan *LiveInfo, 64),
+		lives:   map[string]*LiveInfo{},
+	}
+	go manager.run()
+	e := echo.New()
+	e.GET("/", handlerIndex)
+	e.GET("/on_publish", handlerOnPublish)
+	e.GET("/on_publish_done", handlerOnPublishDone)
+	e.Logger.Fatal(e.Start(":8080"))
+
+}
+
+func handlerIndex(c echo.Context) error {
+	lives := make([]*LiveInfo, 0, len(manager.lives))
+	for _, l := range manager.lives {
+		lives = append(lives, l)
+	}
+	body, err := templateIndex.Execute(
+		pongo2.Context{
+			"lives": lives,
+		},
+	)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+	}
+	return c.HTML(http.StatusOK, body)
+}
+
+func handlerOnPublish(c echo.Context) error {
+	manager.publish <- GetLiveInfoFromContext(c)
+	return c.String(http.StatusOK, "")
+}
+
+func handlerOnPublishDone(c echo.Context) error {
+	manager.done <- GetLiveInfoFromContext(c)
+	return c.String(http.StatusOK, "")
+}
+
+func GetLiveInfoFromContext(c echo.Context) *LiveInfo {
+	app := c.QueryParam("app")
+	name := c.QueryParam("name")
+	return &LiveInfo{
+		app:  app,
+		name: name,
+	}
+}
